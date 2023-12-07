d695b096730f05f4b20ed8909453c1a20577bbca
daone <jcn07253@gmail.com>
2023-12-07 09:33:54 +0900

    setting

---
+++ src/main/java/yu/favourite/FavouriteApplication.java
@@ -0,0 +1,13 @@
+package yu.favourite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FavouriteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FavouriteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/yu/favourite/FavouriteApplicationTests.java
@@ -0,0 +1,13 @@
+package yu.favourite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FavouriteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
