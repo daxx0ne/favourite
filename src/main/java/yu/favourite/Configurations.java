aee78efeaf73f4a984b2aca546f0488e4651e757
daone <jcn07253@gmail.com>
2023-12-14 14:44:49 +0900

    fix: 버그 수정

---
+++ src/main/java/yu/favourite/Configurations.java
@@ -0,0 +1,28 @@
+package yu.favourite;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.SessionCookieConfig;
+import jakarta.servlet.SessionTrackingMode;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Collections;
+
+@Configuration
+public class Configurations implements WebMvcConfigurer {
+
+    @Bean
+    public ServletContextInitializer clearJsession() {
+        return new ServletContextInitializer() {
+            @Override
+            public void onStartup(ServletContext servletContext) throws ServletException {
+                servletContext.setSessionTrackingModes(Collections.singleton(SessionTrackingMode.COOKIE));
+                SessionCookieConfig sessionCookieConfig=servletContext.getSessionCookieConfig();
+                sessionCookieConfig.setHttpOnly(true);
+            }
+        };
+    }
+}
